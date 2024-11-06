3724065b6b3d962b9a31cc64432b5697bf92227e
vinn-as1999 <vinicyus.alves302@gmail.com>
2024-11-06 10:03:00 -0300

    insert products function update

--- app/models/products_models.py
+++ app/models/products_models.py
@@ -3,15 +3,59 @@ from bson.objectid import ObjectId
 from flask import g
 
 class Products:
+    NO_DATA_ERROR = "Campos vazios! Insira dados válidos!"
+    EXISTING_DATA_ERROR = "Dados já existentes"
+    DATA_SUCCESS_MESSAGE = "Dados inseridos com sucesso!"
+
     @staticmethod
     def get():
         # get all products
         pass
 
     @staticmethod
-    def insert():
+    def insert(data):
         # insert products 
-        pass
+        if not data.get("user_id") or not data.get("username") or not data.get("category") or not data.get("product"):
+            return {
+                "error": Products.NO_DATA_ERROR
+            }
+        
+        existing_product = g.db["products"].find_one({
+            "category": data.get("category"), 
+            "products": {
+                "$elemMatch": {
+                    "name": data.get("product")
+                }
+            }
+        })
+
+        if existing_product:
+            g.db["products"].update_one({
+                "category": data.get("category"),
+                "products.name": data.get("product")
+                },
+                {"$inc": {
+                    "products.$.quantity": data.get("quantity")
+                }
+            })
+
+        else:
+            g.db["products"].update_one({
+                "user_id": data.get("user_id"),
+                "username": data.get("username"),
+                "category": data.get("category")
+            }, {"$push": {
+                "products": {
+                    "name": data.get("product"),
+                    "price": data.get("price"),
+                    "quantity": data.get("quantity")
+                }
+            }}, upsert=True)
+
+            return {
+                "success": Products.DATA_SUCCESS_MESSAGE
+            }
+
 
     @staticmethod
     def update():
--- app/routes/products_routes.py
+++ app/routes/products_routes.py
@@ -8,7 +8,27 @@ def get_products():
     # get all products
     pass
 
+
 @products_bp.route('/api/products', methods=["POST"])
 def insert_products():
     # insert products
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "Dados não fornecidos"})
+    
+    result = Products.insert(data)
+    if "error" in result:
+        return jsonify(result), result.get("status", 400)
+    
+    return jsonify(result), 201
+
+
+@products_bp.route('/api/products', methods=["PATCH"])
+def update_products():
     pass
+
+
+@products_bp.route('/api/products', methods=["DELETE"])
+def delete_products():
+    pass
+
