78704f209dc89abd9285adf777cc0ab08fd585b7
vinn-as1999 <vinicyus.alves302@gmail.com>
2024-11-13 11:02:30 -0300

    insert inventory function creation

--- app/models/inventory_models.py
+++ app/models/inventory_models.py
@@ -9,17 +9,52 @@ class Inventory:
     SUCESS_MESSAGE = 'Programa executado com sucesso!'
 
     @staticmethod
-    def insert():
-        pass
+    def validation(data):
+        if not data.get("user_id") or not data.get("username") or not data.get("inventory"):
+            return { "error": Inventory.NO_DATA_ERROR }
+        else:
+            return data
 
     @staticmethod
-    def update():
-        pass
+    def insert(data):
+        existing_data = Inventory.validation(data)
+
+        if "error" in existing_data:
+            return existing_data, 400
+        
+        filt = {
+            "user_id": data.get("user_id"),
+            "username": data.get("username"),
+        }
+
+        try:
+            for key, value in data.get("inventory"):
+                if value is not None:
+                    g.db["inventories"].update_one(
+                        filt,
+                        {
+                            "$set": {
+                                "inventory": {
+                                    key: value
+                                }
+                            }
+                        },
+                        upsert=True
+                    )
+
+        except Exception as error:
+            return {
+                "error": str(error)
+            }
 
     @staticmethod
-    def delete():
-        pass
+    def update(data):
+        existing_data = Inventory.validation(data)
 
     @staticmethod
-    def get():
-        pass
+    def delete(data):
+        existing_data = Inventory.validation(data)
+
+    @staticmethod
+    def get(data):
+        existing_data = Inventory.validation(data)
--- app/routes/inventory_routes.py
+++ app/routes/inventory_routes.py
@@ -7,13 +7,21 @@ FALSY_DATA_ERROR = 'Dados inválidos! Insira dados válidos'
 NO_DATA_ERROR = 'Dados inexistentes! Preencha os campos'
 SUCESS_MESSAGE = 'Programa executado com sucesso!'
 
-@inventory_bp.route('/api/inventory', methods=["GET"])
-def get_inventory():
+@inventory_bp.route('/api/inventory/<string:user_id>', methods=["GET"])
+def get_inventory(user_id):
     pass
 
 @inventory_bp.route('/api/inventory', methods=["POST"])
 def insert_inventory():
-    pass
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": NO_DATA_ERROR})
+    
+    response = Inventory.insert(data)
+    if "error" in response:
+        return jsonify(response), response.get("status", 400)
+    
+    return jsonify(response), 201
 
 @inventory_bp.route('/api/inventory', methods=["PATCH"])
 def update_inventory():
